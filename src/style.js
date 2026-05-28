bebfe3640a523b1e6f8723818ebc3e5ae0d71ec5
3nly <59023753+3nly@users.noreply.github.com>
2026-05-28 10:32:35 +0100

    Remove Native.css -> Original.css

--- src/style.js
+++ src/style.js
@@ -8,15 +8,13 @@
 <%= grunt.file.read('src/css/Quickreply.css') %>
 <%= grunt.file.read('src/css/Icons.css') %>
 <%= grunt.file.read('src/css/Highlight.css') %>
-<%= grunt.file.read('src/css/Options.css') %>
 <%= grunt.file.read('src/css/Catalog.css') %>
 <%= grunt.file.read('src/css/Notifications.css') %>
 /* Notifications*/
 " + ($SS.conf["Center Notifications"] ? "#stylechan-notifications{right:auto;left:50%;transform:translateX(-50%);width:min(500px,calc(100vw - 20px))}.chX-notifs #notifications{right:auto!important;top:30px!important;left:50%!important;transform:translateX(-50%)!important;width:min(480px,calc(100vw - 20px))!important}" : "") + "
 " + ($SS.conf["Full Border"] ? "#stylechan-notifications .stylechan-notification,.chX-notifs #notifications .notification{border:" + $SS.conf["Width Decoration"] + "px " + $SS.conf["Highlight Style"] + " !important}#stylechan-notifications .stylechan-notification-info,.chX-notifs #notifications .notification.info{border-color:#6f8fb3!important}#stylechan-notifications .stylechan-notification-warning,.chX-notifs #notifications .notification.warning{border-color:#c7a85a!important}#stylechan-notifications .stylechan-notification-error,.chX-notifs #notifications .notification.error{border-color:#c06b6b!important}#stylechan-notifications .stylechan-notification-success,.chX-notifs #notifications .notification.success{border-color:var(--sc-quoteColor)!important}" : "") + "
-
-<%= grunt.file.read('src/css/Native.css') %>
 /* Scrollbars */
 " + ($SS.conf["Style Scrollbars"] ? "*{scrollbar-color:" + $SS.theme.brderColor.hex + " " + $SS.theme.mainColor.hex + "}" : "") + "
 " + ($SS.conf["Style Scrollbars"] && $SS.conf["Thin Scrollbars"] ? "*{scrollbar-width:thin}" : "") + "
 /* Leave one at the bottom otherwise file format breaks */
+<%= grunt.file.read('src/css/Options.css') %>
