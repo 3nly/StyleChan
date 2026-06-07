e0702d3c72419c56bbb8bb518ef90b1b5dc2c4b7
3nly <59023753+3nly@users.noreply.github.com>
2026-06-07 03:08:06 +0100

    Revert (you) toggle

--- src/script.js
+++ src/script.js
@@ -3834,9 +3834,9 @@
                 cl.toggle("catalog-background", $SS.conf["Show Background"] === true);
                 cl.toggle("catalog-thumbsize", $SS.conf["Unified Thumbnail Size"] === true);
                 cl.toggle("use-sc-icons", $SS.conf["Use StyleChan Icons"]);
-                cl.toggle("highlight-you", $SS.conf["Highlight Posts Quoting You"] === true);
-                cl.toggle("highlight-own", $SS.conf["Highlight Own Posts"] === true);
                 if (!$SS.is4chanX()) {
+                    cl.toggle("highlight-you", $SS.conf["Highlight Posts Quoting You"] === true);
+                    cl.toggle("highlight-own", $SS.conf["Highlight Own Posts"] === true);
                     if ($SS.conf["Highlight Own Posts"]) $SS.markOwnPosts();
                     if ($SS.conf["Highlight Posts Quoting You"]) $SS.markQuotingYou();
                     if ($SS.conf["Relative Post Dates"]) $SS.relativeDates();
